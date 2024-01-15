ab50736a0958f7dd8217aadaf38cc11d9527c8e0
Nalou Nguyen <nalou.dev@gmail.com>
2024-01-15 17:42:06 +0700

    update code

--- Pods/Target Support Files/Pods-BrigewellSdk-BrigewellSdkTests/Pods-BrigewellSdk-BrigewellSdkTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BrigewellSdk_BrigewellSdkTests : NSObject
-@end
-@implementation PodsDummy_Pods_BrigewellSdk_BrigewellSdkTests
-@end
--- Pods/Target Support Files/Pods-BrigewellSdk/Pods-BrigewellSdk-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BrigewellSdk : NSObject
-@end
-@implementation PodsDummy_Pods_BrigewellSdk
-@end
